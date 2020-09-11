6a848d6c511a173affd092c40c0a8eed9fd4baff
Nick Young <nick.young@stacktracehq.com>
2020-09-11 16:15:37 +1000

    Export dotfiles/linux/bin to $PATH

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -1,8 +1,8 @@
-set -x dotfiles_root (realpath (dirname (status --current-filename))/../)
+set -xg dotfiles_root (realpath (dirname (status --current-filename))/../)
+set -xg PATH "$dotfiles_root/bin" $PATH
 
 starship init fish | source
 
 source $dotfiles_root/fish/git.fish
 source $dotfiles_root/fish/aliases.fish
 source $dotfiles_root/fish/nix.fish
-
