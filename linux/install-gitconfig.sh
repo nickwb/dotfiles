faff9c88f1f7deb109cd6e489c0a1a5fa320f12d
Nick Young <nick.young@stacktracehq.com>
2020-10-20 14:07:50 +1000

    Change .gitconfig to a symlink into dotfiles

--- linux/install-gitconfig.sh
+++ linux/install-gitconfig.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-cp .gitconfig ~/.gitconfig
+
+CONFIG_PATH="$(realpath $(dirname "$0"))/.gitconfig";
+LINK_PATH="$HOME/.gitconfig";
+
+if [ -f "$LINK_PATH" ]; then
+    echo "Removing: $LINK_PATH";
+    rm $LINK_PATH;
+fi
+
+echo "Linking: $CONFIG_PATH";
+ln -s $CONFIG_PATH $LINK_PATH;
