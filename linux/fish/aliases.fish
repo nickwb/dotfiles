7465205fa86d145d13a3dfe4bba4bc55d61b85ec
Nick Young <nick@nickwb.net>
2021-06-01 10:11:55 +1000

    Handle `fd` being named either `fd` or `fdfind`

--- linux/fish/aliases.fish
+++ linux/fish/aliases.fish
@@ -1,7 +1,10 @@
 alias cat='bat --theme=OneHalfDark'
 alias ls='exa'
 alias fuu='killall -9 rider'
-alias fd='fdfind'
 alias fcd='cd (fd -t d | fzf)'
 alias top='procs --watch --sortd cpu'
 alias y='maybe-yarn'
+
+if which fdfind
+    alias fd='fdfind'
+end
