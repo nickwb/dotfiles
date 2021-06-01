840ad12ae2a3f28706479a0505f27851262cf29f
Nick Young <nick@nickwb.net>
2021-06-01 11:12:40 +1000

    Alias `open` to `explorer.exe` on WSL2

--- linux/fish/aliases.fish
+++ linux/fish/aliases.fish
@@ -5,6 +5,12 @@ alias fcd='cd (fd -t d | fzf)'
 alias top='procs --watch --sortd cpu'
 alias y='maybe-yarn'
 
-if which fdfind
+# The name of fd is different depending on your package source...
+if which fdfind > /dev/null
     alias fd='fdfind'
+end
+
+# Map open to explorer on WSL2
+if which explorer.exe > /dev/null
+    alias open='explorer.exe'
 end
