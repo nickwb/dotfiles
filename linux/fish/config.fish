55743f33d291e2434ce9dd6f3f9f9d335b871ed5
Nick Young <nick@nickwb.net>
2022-01-17 13:57:35 +1000

    Seperate out WSL binaries, and support xdg-open

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -1,6 +1,10 @@
 set -xg dotfiles_root (realpath (dirname (status --current-filename))/../)
 set -xg PATH "$dotfiles_root/bin" "$HOME/.cargo/bin" $PATH
 
+if test 1 -eq $NICK_USE_WSL;
+  set -xg PATH "$dotfiles_root/bin-wsl" $PATH
+end
+
 starship init fish | source
 
 source $dotfiles_root/fish/git.fish
--- linux/fish/machine.lenovo.wsl2.fish
+++ linux/fish/machine.lenovo.wsl2.fish
@@ -1,2 +1,3 @@
 set -xg NICK_CODE_ROOT /mnt/c/Users/NickYoung/code/
 set -xg NICK_USE_NIX 0
+set -xg NICK_USE_WSL 1
